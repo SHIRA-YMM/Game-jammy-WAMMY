40c2614a3296a8b112c6a778f405534dbdb5d626
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2025-11-07 14:52:05 +0700

    fix animation

--- Assets/Scripts/Dialog/intro.cs
+++ Assets/Scripts/Dialog/intro.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class intro : MonoBehaviour
 {
+    public GameObject fadein;
     DialogueManager dialogueManager;
     private void Awake() {
         dialogueManager = FindObjectOfType<DialogueManager>();
@@ -10,8 +13,15 @@ public class intro : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        StartCoroutine(startAnimation());
         dialogueManager.StartDialogueByIndex(0);
     }
 
-    
+    IEnumerator startAnimation()
+    {
+        fadein.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        fadein.SetActive(false);
+    }
+
 }
